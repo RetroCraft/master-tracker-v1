06d77063e434b5c84c7e95cd4c010981ed4536d7
octoshrimpy <octavo.acapella@gmail.com>
2018-12-07 16:19:27 -0700

    sidebar works sort of

--- site/main.js
+++ site/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
 
+    // setup nav
     $('.nav-page').each(function(){
         console.log(`cleaned ${$(this).data('page')}!`)
         $(this).data('page', $(this).data('page'))
         $(this).removeAttr('data-page')
     })
 
+    // navigate
     $(document).on('click', '.nav-page', function(){
         $('.nav-page.is-active').removeClass('is-active')
         $(this).addClass('is-active')
     })
+
+    // collapse sections
+    $(document).on('click', '.menu-header .icon', function(){
+        console.log($(this))
+
+        $(this).parents('.menu-list').next('.collapse-me').slideToggle()
+        $(this).parents('.menu-header').toggleClass('is-expanded')
+    })
+
+    // create sidebar
+    // UNUSED FOR NOW
+    $.getJSON('./config/sidebar.json', function(data){
+
+    })
+
+
 })
 
 function grabPage(loc){
